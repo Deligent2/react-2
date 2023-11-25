0d0c86515ea6af8d540e148d3571ab7682b43ba2
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-11-25 15:14:55 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [state, setState] = useState(0)
+    const [inputValue, setInput] = useState('')
+
+    const handleCalculator = (num)=>{
+        if(num==='C'){
+            setState(0)
+            setInput('')
+        }else{
+            setState(eval(inputValue));
+            setInput(`${eval(inputValue)}${num}`);
+        }
+
+    }
+
+    const handleNumber=(num)=>{
+        setInput(`${inputValue}${num}`);
+
+    }
+
+    return (
+        <div className={'container'}>
+            <input type="text" value={inputValue}/>
+            <div className={'container-wrapper'}>
+               <div className={'btn-wrap'}>
+                   <button className={'number'} onClick={()=>handleNumber(1)}>1</button>
+                   <button className={'number'} onClick={()=>handleNumber(2)}>2</button>
+                   <button className={'number'} onClick={()=>handleNumber(3)}>3</button>
+                   <button className={'symbol'} onClick={()=>handleCalculator('+')}>+</button>
+               </div>
+
+
+
+             <div className={'btn-wrap'}>
+                 <button className={'number'} onClick={()=>handleNumber(4)}>4</button>
+                 <button className={'number'} onClick={()=>handleNumber(5)}>5</button>
+                 <button className={'number'} onClick={()=>handleNumber(6)}>6</button>
+                 <button className={'symbol'} onClick={()=>handleCalculator('-')}>-</button>
+             </div>
+               <div className={'btn-wrap'}>
+
+                   <button className={'number'} onClick={()=>handleNumber(7)}>7</button>
+                   <button className={'number'} onClick={()=>handleNumber(8)}>8</button>
+                   <button className={'number'} onClick={()=>handleNumber(9)}>9</button>
+                   <button className={'symbol'} onClick={()=>handleCalculator('*')}>*</button>
+               </div>
+               <div className={'btn-wrap'}>
+                   <button className={'symbol'} onClick={()=>handleCalculator('C')}>C</button>
+                   <button className={'number'} onClick={()=>handleNumber(0)}>0</button>
+                   <button className={'symbol'} onClick={()=>handleCalculator(' ')}>=</button>
+                   <button className={'symbol'} onClick={()=>handleCalculator('/')}>/</button>
+
+               </div>
+
+
+
+
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import './style.css'
+//
+//
+// const Btn = ({text})=>{
+//
+//     const handleClick = ()=>{
+//         console.log(`Hello ${text}`)
+//     }
+//     return(
+//
+//             <button className={'customsButton'} onClick={handleClick}>{text}</button>
+//     )
+// }
+// export default Btn
+//
+// export const Input = ()=>{
+//     const handleChange=(e)=>{
+//         console.log(e.target.value)
+//
+//     }
+//     return (
+//         <input onChange={handleChange} placeholder={'Its my input'} value={'Input'}/>
+//     )
+// }
